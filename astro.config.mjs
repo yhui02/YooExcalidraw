01abe5dcb3209a6e73acf7e33c902a44b71a79d0
Yohann <yhui02@gmail.com>
2026-06-28 10:14:13 +0800

    交互优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+var base = process.env.GITHUB_ACTIONS ? '/YooExcalidraw' : '/';
+
 export default defineConfig({
   site: 'https://yhui02.github.io',
-  base: '/YooExcalidraw',
+  base: base,
   output: 'static',
   integrations: [react()],
   devToolbar: { enabled: false },
